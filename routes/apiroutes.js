0853eae5fd980e760cb0e89807586eaa62a158d1
ericolson1977 <mr_tues_night@yahoo.com>
2023-10-20 01:00:26 -0500

    finshed codind and modularizing

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -11,13 +11,13 @@ apiRoutes.get('/notes', (req, res) => {
         }
         const dbData = JSON.parse(data);
         res.json(dbData);
-    })
+    });
 })
 
 apiRoutes.post('/notes', (req, res) => {
     console.info(`${req.method} request received to add a note`);
     const { title, text, id } = req.body;
-    if (title && text ) {
+    if (title && text) {
         const newNote = {
             title,
             text,
@@ -29,20 +29,24 @@ apiRoutes.post('/notes', (req, res) => {
                 console.log(err);
             } else {
                 const note = JSON.parse(data);
-                note.push(newNote);
-                fs.writeFile(`db/db.json`, JSON.stringify(note, null, 2), (err) => {
-                    if (err) {
-                        console.error(err);
-                        return res.status(500).json({ error: 'Failed to write data.' });
-                    }
-                    console.log(`New note has been written to JSON file`);
-                    res.json(newNote);
-                });
+        note.push(newNote);
+        fs.writeFile(`db/db.json`, JSON.stringify(note, null, 2), (err) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Failed to write data.' });
             }
+            console.log(`New note has been written to JSON file`);
+            res.json(newNote);
         });
-    } else {
-        res.status(400).json({ error: 'Title and text are required.' });
     }
 });
+    } else {
+    res.status(400).json({ error: 'Title and text are required.' });
+}
+});
+
+// apiRoutes.delete('notes/:id', (req, res) => {
+    
+// });
 
 module.exports = apiRoutes;
